fa4692cc77ad2e451f259f947f48c9771ca2f282
Mildman <mildman@mailbox.org>
2025-06-24 23:15:49 +0200

    Fixing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.mildman1848.sachkundenachweis"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
